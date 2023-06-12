d167d4b2268a95213ea42b1ff59c4fa477dffb5a
Raul Burgos <specializeddevel@gmail.com>
2023-06-12 19:22:15 -0400

    Configuracion del proyecto

--- SistemaInventario.Data/Data/ApplicationDbContext.cs
+++ SistemaInventario.Data/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Model;
+using System.Reflection;
 
 namespace SistemaInventario.Data.Data
 {
@@ -8,6 +10,14 @@ namespace SistemaInventario.Data.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
---
+++ SistemaInventario.Model/Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Model
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "Se admiten como máximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [MaxLength(100, ErrorMessage = "Se admiten como máximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set;}
+
+    }
+}
